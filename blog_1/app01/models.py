1a7f1ffffe3f802604617f570dffeea163a4a1bf
Chen junyi <314298729@qq.com>
2022-06-13 18:28:24 +0800

    完成数据结构

--- blog_1/app01/models.py
+++ blog_1/app01/models.py
@@ -165,3 +165,225 @@ class Comment(models.Model):
 
     class Meta:
         verbose_name_plural = '评论'
+
+
+# 新闻爬取记录
+class New(models.Model):
+    nid = models.AutoField(primary_key=True)
+    # 注意auto_now_add是固定的 auto_now是变化的
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='获取时间')
+
+    class Meta:
+        verbose_name_plural = '新闻爬取'
+
+
+# 文章封面
+class Cover(models.Model):
+    nid = models.AutoField(primary_key=True)
+    url = models.FileField(verbose_name='文章封面地址', upload_to='article_img/')
+
+    def __str__(self):
+        return str(self.url)
+
+    class Meta:
+        verbose_name_plural = '文章封面'
+
+
+@receiver(pre_delete, sender=Cover)  # sender你要删除或修改文件字段所在的类
+def Cover_delete(instance, **kwargs):  # 函数名随意
+    # print('进入文件删除方法，删除的是' ,instance.url)
+    instance.url.delete(False)  # file是保存文件或图片的字段名
+
+
+# 标签表
+class Tags(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=16, verbose_name='标签名字')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = '文章标签'
+
+
+# 回忆录
+class History(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=32, verbose_name='事件名称')
+    content = models.TextField(verbose_name='事件内容')
+    create_date = models.DateField(verbose_name='创建时间', null=True)
+    drawing = models.TextField(verbose_name='配图组，以;隔开', null=True, blank=True)
+    class Meta:
+        verbose_name_plural = '回忆录'
+
+#心情
+class Moods(models.Model):
+    nid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16, verbose_name='发布人')
+    create_date = models.DateTimeField(verbose_name='发布时间',auto_now=True)
+    content = models.TextField(verbose_name='心情内容')
+    drawing = models.TextField(verbose_name='配图组，以;隔开', null=True, blank=True)
+    comment_count = models.IntegerField(verbose_name='评论数',default=0)
+    digg_count = models.IntegerField(verbose_name='评论数',default=0)
+    avator = models.ForeignKey(
+        to = 'Avatars',
+        to_field='nid',
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='心情发布的头像'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = '心情'
+
+#心情评论
+class MoodComment(models.Model):
+    nid = models.AutoField(primary_key=True)
+    avatar = models.ForeignKey(
+        to = 'Avatars',
+        to_fields= 'nid',
+        on_delete= models.SET_NULL,
+        null = True,
+        verbose_name='心情发布头像'
+    )
+    name = models.CharField(max_length=16,verbose_name='评论人',null=True)
+    content = models.TextField(verbose_name='评论内容')
+    digg_count = models.IntegerField(verbose_name='点赞数',default=0)
+    mood = models.ForeignKey(
+        to='Moods',
+        to_field = 'nid',
+        on_delete= models.SET_NULL,
+        verbose_name='评论心情',
+        null=True
+    )
+    create_date = models.DateTimeField(verbose_name='评论时间',auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name_plural = '心情评论'
+
+#网站导航
+class Navs(models.Model):
+    nid = models.AutoField(primary_key=True)
+    nav_category = models.ForeignKey(
+        to='NavCategory',
+        to_field='nid',
+        on_delete=models.SET_NULL,
+        verbose_name='网站导航分类',
+        null = True
+    )
+    icon_href = models.URLField(verbose_name='图片链接',help_text='在线连接',null=True,blank=True)
+    icon = models.FileField(verbose_name='网站图标',null=True,blank=True,
+                            upload_to='site_icon/',help_text='文件优先级大于在线链接')
+    title = models.CharField(max_length=32,verbose_name='网站标题')
+    abstract = models.CharField(max_length=128,verbose_name='网站简介',null=True)
+    create_date = models.DateTimeField(verbose_name='创建时间',auto_now=True)
+    href = models.URLField(verbose_name='网站链接')
+    status_choice = (
+        (0,'待审核'),
+        (1,'已通过'),
+        (2,'被驳回')
+    )
+    status = models.IntegerField(verbose_name='导航状态',choices=status_choice,default=0)
+
+    #定义一个函数
+    def color_state(self):
+        if self.status == 0:
+            assign_state_name = '待审核'
+            color_code = '#ec921e'
+        elif self.status == 1:
+            color_code = 'green'
+            assign_state_name = '已通过'
+        else:
+            color_code = 'red'
+            assign_state_name = '被驳回'
+        return format_html(
+            '<span style="color:{};">{}</span>'
+        )
+    color_state.short_description = '导航状态'
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = '网站导航'
+
+#导航分类
+class NavCategory(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=16,verbose_name='分类标题')
+    icon = models.CharField(verbose_name='分类图标',max_length=32)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = '导航分类'
+
+#站点背景
+class Menu(models.Model):
+    nid = models.AutoField(primary_key=True)
+    menu_title = models.CharField(max_length=16, verbose_name='菜单名称')
+    menu_title_en = models.CharField(max_length=32, verbose_name='菜单英文名字')
+    title = models.CharField(max_length=32, verbose_name='slogen', null=True)
+    abstract = models.TextField(verbose_name='slogen介绍', help_text='多个之间按分号区分', null=True)
+    abstract_time = models.IntegerField(verbose_name='slogen切换时间', help_text='单位秒，默认8秒', default=8)
+    rotation = models.BooleanField(verbose_name='是否轮播slogen介绍', default=True)
+    menu_url = models.ManyToManyField(
+        to='MenuImg',
+        verbose_name='菜单图片',
+        help_text='可以多选，多选就会轮播',
+    )
+    menu_rotation = models.BooleanField(verbose_name='是否轮播banner图', help_text='多选默认会轮播', default=False)
+    menu_time = models.IntegerField(verbose_name='轮播图切换时间', default=8, help_text='单位秒,默认为8秒')
+
+    class Meta:
+        verbose_name_plural = '站点背景'
+
+
+# 站点背景图
+class MenuImg(models.Model):
+    nid = models.AutoField(primary_key=True)
+    url = models.FileField(verbose_name='图片地址', upload_to='site_bg/')
+
+    def __str__(self):
+        return str(self.url)
+
+    class Meta:
+        verbose_name_plural = '站点背景图'
+
+
+# 广告表
+class Advert(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=32, null=True)
+    href = models.URLField(verbose_name='跳转链接')
+    img = models.FileField(verbose_name='图片地址', null=True, blank=True, help_text='单图', upload_to='advert/')
+    img_list = models.TextField(verbose_name='图片组', null=True, blank=True, help_text='上传图片请用线上地址，使用:隔开多张图片')
+    is_show = models.IntegerField(verbose_name='是否展示', default=False)
+    author = models.CharField(verbose_name='广告主', max_length=32, null=True, blank=True)
+    abstract = models.CharField(verbose_name='产品介绍', max_length=128, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = '广告'
+
+
+# 反馈信息
+class Feedback(models.Model):
+    nid = models.AutoField(primary_key=True)
+    email = models.EmailField(verbose_name='邮箱')
+    content = models.TextField(verbose_name='反馈信息')
+    status = models.BooleanField(verbose_name='是否处理', default=False)
+    processing_content = models.TextField(verbose_name='回复的内容', null=True, blank=True)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name_plural = '用户反馈'
