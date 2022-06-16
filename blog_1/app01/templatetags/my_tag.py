e034c7071f18f2b7d761a80708e4ee8be5136809
Chen junyi <314298729@qq.com>
2022-06-16 18:15:24 +0800

    所有文章列表

--- blog_1/app01/templatetags/my_tag.py
+++ blog_1/app01/templatetags/my_tag.py
@@ -6,8 +6,8 @@ register = template.Library()
 @register.inclusion_tag('my_tag/headers.html')
 def banner(menu_name):
     img_list = [
-        'static/my/img/banner/banner1.jpg',
-        'static/my/img/banner/banner2.jpg',
+        '/static/my/img/banner/banner1.jpg',
+        '/static/my/img/banner/banner2.jpg',
     ]
     print(menu_name)
     return {'img_list': img_list}
--- blog_1/blog_1/urls.py
+++ blog_1/blog_1/urls.py
@@ -1,12 +1,12 @@
-
 from django.contrib import admin
-from django.urls import path,re_path
-from django.views.static import serve
-from django.conf import settings
+from django.urls import path, re_path
+# from django.views.static import serve
+# from django.conf import settings
 from app01 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
-    path('',views.index),
+    # re_path(r'media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    path('', views.index),
+    path('news/', views.news),
 ]
